da2a8b03601294227575389093dc40da3a24a2dd
Morgadineo <arthurmorgado7751@gmail.com>
2025-05-18 16:05:22 -0300

    Fiat Lux

---
+++ README.md
@@ -0,0 +1,3 @@
+# Compactador de Huffman.
+
+Um código Huffman é um tipo específico de código de prefixo ótimo comumente usado para compressão de dados sem perdas. O processo de encontrar ou usar tal código é a codificação Huffman, um algoritmo desenvolvido por David A. Huffman enquanto ele era aluno de doutorado no MIT e publicado no artigo de 1952 "Um Método para a Construção de Códigos de Redundância Mínima".
---
+++ main.py
@@ -0,0 +1,33 @@
+###############################################################################
+#
+# -> Compactador de Huffman <-
+# 
+# Grupo  : A
+# Membros: Arthur Morgado Teixeira,
+#              ...,
+#              ...,
+#
+# Data   : 18/06/2025
+###############################################################################
+
+class HuffmanCompacter:
+    """
+    Um código Huffman é um tipo específico de código de prefixo ótimo comumente
+    usado para compressão de dados sem perdas. O processo de encontrar ou usar 
+    tal código é a codificação Huffman, um algoritmo desenvolvido por David A. 
+    Huffman enquanto ele era aluno de doutorado no MIT e publicado no artigo 
+    de 1952 "Um Método para a Construção de Códigos de Redundância Mínima".
+    
+    O compactador deve funcionar para qualquer tipo arquivo, independente da
+    extensão.
+
+    """
+    def __init__(self) -> None:
+        pass
+
+if __name__ == "__main__":
+    # Área de Execução
+    compacter: HuffmanCompacter = HuffmanCompacter()
+
+
+
